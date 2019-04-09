e77b06cf6c22aa19151b3e0412f57774cd61c641
Hironichu <nzenzela@student.le-101.fr>
2019-04-09 20:16:35 +0200

    qwdwefwef

--- incs/defines.h
+++ incs/defines.h
@@ -6,15 +6,15 @@
 /*   By: nzenzela <nzenzela@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/05 18:40:55 by jfeve        #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/09 15:18:18 by nzenzela    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/09 20:00:43 by nzenzela    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
-# define WIN_W 1500
-# define WIN_H 1000
+# define WIN_W 1000
+# define WIN_H 600
 # define UNIT 15
 # define HUD_BEGIN 851
 # define PIXFOR SDL_PIXELFORMAT_RGBA8888
