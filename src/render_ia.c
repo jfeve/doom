394ae6ecb61483c22d0523425154701083a62c43
flombard <florianlombardo3@gmail.com>
2019-05-07 14:05:24 +0200

    armor now lowers damage a little

--- src/render_ia.c
+++ src/render_ia.c
@@ -6,7 +6,7 @@
 /*   By: flombard <flombard@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/05/06 19:03:46 by flombard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/07 13:47:17 by flombard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/05/07 14:05:06 by flombard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,17 +15,19 @@
 
 void	enemy_ia(t_mapf *mapf, t_hud *hud)
 {
-	short i;
-	short j;
-	float res;
+	short	i;
+	short	j;
+	float	res;
+	int		taken_dmg;
 
+	taken_dmg = 0;
 	if ((i = mapf->nbrend_s) > MAX_SECT)
 		i = MAX_SECT;
 	while (--i >= 0)
 	{
 		j = -1;
 		t_sector	now = mapf->sectors[mapf->rend_s[i].id];
-		while (++j < now.nbenem)
+		while (++j < now.nbenem && j < MAX_SPRITE)
 		{
 			if ((res = vector_measure(now.enem[j].x, 
 			now.enem[j].y, mapf->player.where.x,
@@ -49,65 +51,20 @@ void	enemy_ia(t_mapf *mapf, t_hud *hud)
 					}
 				}
 				else if (mapf->player.life != 0)
-				{
-					mapf->player.life--;
-					SDL_FreeSurface(hud->nblife);
-					if (!(hud->nblife = init_text(hud->arial, ft_itoa(mapf->player.life), mapf->sdl.form->format, (SDL_Color){0, 0, 0, 255})))
-					{
-						free_hud(hud);
-						return ;
-					}
-				}
+					taken_dmg++;
 			}
 		}
 	}
-}
-
-	/*short		i;
-	int			j;
-	int			nbdraw;
-	t_sprite	drawable[MAX_SPRITE];
-
-	if ((i = mapf->nbrend_s) > MAX_SECT)
-		i = MAX_SECT;
-	while (--i >= 0)
+	if (taken_dmg > 0)
 	{
-		nbdraw = 0;
-		ft_bzero(drawable, MAX_SPRITE * sizeof(t_sprite));
-		t_sector now = &mapf->sectors[mapf->rend_s[i].id];
-		nbdraw = go_through_enemies(now, mapf->player, drawable, nbdraw);
-		j = -1;
-		while (++j < nbdraw && j < MAX_SPRITE)
+		if (hud->has_armor == 1 && taken_dmg > 1)
+			taken_dmg = 2 * taken_dmg / 3;
+		mapf->player.life -= taken_dmg;
+		SDL_FreeSurface(hud->nblife);
+		if (!(hud->nblife = init_text(hud->arial, ft_itoa(mapf->player.life), mapf->sdl.form->format, (SDL_Color){0, 0, 0, 255})))
 		{
-			if ((res = vector_measure(mapf->sectors[i].enem[j].x, 
-			mapf->sectors[i].enem[j].y, mapf->player.where.x,
-			mapf->player.where.y)) > 1.5f && res < 15 &&
-			mapf->sectors[i].enem[j].life != 0)
-			{
-					mapf->sectors[i].enem[j].x += (0.05f / res) * (mapf->player.where.x - mapf->sectors[i].enem[j].x);
-					mapf->sectors[i].enem[j].y += (0.05f / res) * (mapf->player.where.y - mapf->sectors[i].enem[j].y);
-			}
-			else if (vector_measure(mapf->sectors[i].enem[j].x, 
-			mapf->sectors[i].enem[j].y, mapf->player.where.x,
-			mapf->player.where.y) <= 2.0f && mapf->sectors[i].enem[j].life != 0)
-			{
-				if (mapf->player.life == 0)
-				{
-					hud->timer = -1;
-					if (!(hud->text = init_text(hud->arial, "Game Over !", mapf->sdl.form->format, (SDL_Color){255, 255, 255, 255})))
-					{
-						free_hud(hud);
-						return ;
-					}
-				}
-				else if (mapf->player.life != 0)
-					mapf->player.life--;
-				SDL_FreeSurface(hud->nblife);
-				if (!(hud->nblife = init_text(hud->arial, ft_itoa(mapf->player.life), mapf->sdl.form->format, (SDL_Color){0, 0, 0, 255})))
-				{
-					free_hud(hud);
-					return ;
-				}
-			}
+			free_hud(hud);
+			return ;
 		}
-	}*/
+	}
+}
