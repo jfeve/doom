a716bcd53a9a920caac9e0ee05766a64510a03f0
flombard <florianlombardo3@gmail.com>
2019-05-08 19:01:41 +0200

    math_5 norm

--- src/maths_5.c
+++ src/maths_5.c
@@ -6,7 +6,7 @@
 /*   By: flombard <flombard@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/05/07 20:21:59 by flombard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/08 18:30:50 by flombard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/05/08 19:01:27 by flombard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -27,7 +27,7 @@ int		arr(int x)
 	return (x / UNIT);
 }
 
-float		vector_measure(float x1, float y1, float x2, float y2)
+float	vector_measure(float x1, float y1, float x2, float y2)
 {
 	float dx;
 	float dy;
