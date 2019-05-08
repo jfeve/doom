6c49eb4e54aee248aebbca75e6a97ecfe4e4ec8a
flombard <florianlombardo3@gmail.com>
2019-05-08 21:47:07 +0200

    norm

--- incs/doom.h
+++ incs/doom.h
@@ -6,7 +6,7 @@
 /*   By: flombard <flombard@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/04 19:41:06 by jfeve        #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/08 20:21:10 by jfeve       ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/05/08 21:46:38 by flombard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -376,7 +376,8 @@ int								read_sector(int fd, t_mapf *mapf, int i);
 
 int								init_mapf(t_mapf *mapf, char *str);
 void							init_ren(t_mapf *mapf, t_ren *ren);
-void							init_v1v2(t_ren *ren, int s, t_sector *sect, t_mapf *mapf);
+void							init_v1v2(t_ren *ren, int s, t_sector *sect,
+																t_mapf *mapf);
 
 /*
 ** Math functions for integers
@@ -414,14 +415,17 @@ int								check_ps(t_mapf *mapf);
 */
 
 void							fill_head(t_ren *ren, int beginx, int endx);
-void							get_yny(t_ren *ren, t_mapf *mapf, t_sector *sect, int s);
+void							get_yny(t_ren *ren, t_mapf *mapf,
+														t_sector *sect, int s);
 void							get_ycy(t_ren *ren, int x);
 void							get_t1t2(t_ren *ren);
 int								get_x(t_float t);
 void							fill_rends(t_mapf *mapf, t_queue now);
 void							clip_t1t2(t_ren *ren);
-void							draw(t_mapf *mapf, t_point xx, t_point yy, t_sector *sect);
-void							draw_text(t_mapf *mapf, t_ren *ren, t_quad qu, t_sector *sect);
+void							draw(t_mapf *mapf, t_point xx, t_point yy,
+																t_sector *sect);
+void							draw_text(t_mapf *mapf, t_ren *ren,
+													t_quad qu, t_sector *sect);
 
 /*
 ** HUD related functions
